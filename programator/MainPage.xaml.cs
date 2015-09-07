8203d73ee386712f7264437078443f80af411209
Maciej.Baj <macie.baj@gmail.com>
2015-09-07 19:59:42 +0200

    repository setup- working serial port and avrdude as console program

---
+++ programator/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.Generic;
+//using System.Management; // need to add System.Management to your project references
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace programator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ProgrammatorDevice _programmator;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += MainPageLoaded;
+        }
+
+        private void MainPageLoaded(object sender, RoutedEventArgs e)
+        {
+            ProgrammatorDevice.UsbDeviceFound += async (o, args) =>
+            {
+                _programmator = args.ProgrammatorDevice;
+                await _programmator.SendRandomMessage();
+            };
+            ProgrammatorDevice.SearchForUsbDevices(Dispatcher);
+        }
+
+        private static void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+
+            ManagementObjectCollection collection;
+            var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub");
+            collection = searcher.Get();
+
+            foreach (var device in collection)
+            {
+                devices.Add(new USBDeviceInfo(
+                (string)device.GetPropertyValue("DeviceID"),
+                (string)device.GetPropertyValue("PNPDeviceID"),
+                (string)device.GetPropertyValue("Description")
+                ));
+            }
+
+            collection.Dispose();
+
+
+//            await _programmator.SendRandomMessage();
+        }
+    }
+    class USBDeviceInfo
+    {
+        public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+        {
+            this.DeviceID = deviceID;
+            this.PnpDeviceID = pnpDeviceID;
+            this.Description = description;
+        }
+        public string DeviceID { get; private set; }
+        public string PnpDeviceID { get; private set; }
+        public string Description { get; private set; }
+    }
+}
---
+++ programator/ProgrammatorDevice.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.HumanInterfaceDevice;
+using Windows.Foundation;
+using Windows.Storage;
+using Windows.UI.Core;
+
+namespace programator
+{
+    class ProgrammatorDevice
+    {
+        private HidDevice _hidDevice;
+        private ProgrammatorDevice(HidDevice hidDevice)
+        {
+            _hidDevice = hidDevice;
+        }
+
+         private async Task SendOutputMessage(byte[] message)
+        {
+            if (_hidDevice != null)
+            {
+                HidOutputReport report = _hidDevice.CreateOutputReport();
+                report.Data = message.AsBuffer();
+
+                await _hidDevice.SendOutputReportAsync(report);
+            }
+        }
+         private static readonly byte[] Cmd = { 0, 0, 0, 0, 0, 0, 0, 0, 2 };
+         public async Task SendRandomMessage()
+         {
+             await SendOutputMessage(Cmd);
+         }
+
+
+         private const ushort vid = 8483;
+         private const ushort pid = 4112;
+         private const ushort uid = 16;
+         private const ushort uPage = 1;
+
+        public static EventHandler<ProgrammatorDeviceEventArgs> UsbDeviceFound;
+
+        public static void SearchForUsbDevices(CoreDispatcher dispatcher)
+        {
+            DeviceWatcher deviceWatcher =
+                DeviceInformation.CreateWatcher(HidDevice.GetDeviceSelector(uPage, uid));
+            deviceWatcher.Added += (s, a) => dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                HidDevice hidDevice = await HidDevice.FromIdAsync(a.Id, FileAccessMode.ReadWrite);
+                var launcher = new ProgrammatorDevice(hidDevice);
+                if (UsbDeviceFound != null)
+                {
+                    UsbDeviceFound(null, new ProgrammatorDeviceEventArgs(launcher));
+                }
+                  
+                deviceWatcher.Stop();
+            });
+            deviceWatcher.Start();
+        }
+
+        public void Dispose()
+        {
+            _hidDevice.Dispose();
+        }
+
+        ~ProgrammatorDevice()
+        {
+            Dispose();
+        }
+
+
+    }
+
+    internal class ProgrammatorDeviceEventArgs
+    {
+        public ProgrammatorDeviceEventArgs(ProgrammatorDevice programmator)
+        {
+            ProgrammatorDevice = programmator;
+        }
+
+        public ProgrammatorDevice ProgrammatorDevice { get; set; }
+    }
+}
